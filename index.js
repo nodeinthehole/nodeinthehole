271931ed6c6dd9d028ef3eefccc2b314404a0359
Tom Davies <tom.davies@atombank.co.uk>
2021-03-10 17:06:46 +0000

    symbols

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const pkgJson = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
+const sym = require('log-symbols');
+const { red: error, yellow: warning, green: success, blue: info } = chalk;
 
 welcome({
     title: 'nodeinthehole',
@@ -9,3 +12,11 @@ welcome({
     bgColor: '#47d7ac',
     clear: false
 });
+
+console.log(`
+${sym.error} ${error('error')}
+${sym.success} ${success('success')}
+${sym.warning} ${warning('warning')}
+${sym.info} ${info('info')}
+`)
+
