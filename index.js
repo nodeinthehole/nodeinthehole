8d2af491bbea0096a31e720ec14aa1330debbc1c
Tom Davies <tom.davies@atombank.co.uk>
2021-03-11 12:05:13 +0000

    more project setup

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
-const pkgJson = require('./package.json');
-const welcome = require('cli-welcome');
-const chalk = require('chalk');
-const sym = require('log-symbols');
+const packageJson = require("./package.json");
+const welcome = require("cli-welcome");
+const chalk = require("chalk");
+const sym = require("log-symbols");
 const { red: error, yellow: warning, green: success, blue: info } = chalk;
 
 welcome({
-    title: 'nodeinthehole',
-    description: pkgJson.description,
-    version: pkgJson.version,
-    bgColor: '#47d7ac',
-    clear: false
+  title: "nodeinthehole",
+  description: packageJson.description,
+  version: packageJson.version,
+  bgColor: "#47d7ac",
+  clear: false,
 });
 
 console.log(`
-${sym.error} ${error('error')}
-${sym.success} ${success('success')}
-${sym.warning} ${warning('warning')}
-${sym.info} ${info('info')}
-`)
-
+${sym.error} ${error("error")}
+${sym.success} ${success("success")}
+${sym.warning} ${warning("warning")}
+${sym.info} ${info("info")}
+`);
