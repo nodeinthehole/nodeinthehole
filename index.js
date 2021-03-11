e7bc43b78dc00f3233d08483242c602a94fad35a
Tom Davies <tom.davies@atombank.co.uk>
2021-03-11 17:06:16 +0000

    started error handling

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
-const packageJson = require("./package.json");
+const unhandled = require("cli-handle-unhandled");
 const welcome = require("cli-welcome");
 const chalk = require("chalk");
+const checkNode = require("cli-check-node");
 const sym = require("log-symbols");
+const packageJson = require("./package.json");
 const { red: error, yellow: warning, green: success, blue: info } = chalk;
+
+checkNode("10");
+unhandled();
+
+Promise.reject(new Error("FFS Tom"));
 
 welcome({
   title: "nodeinthehole",
